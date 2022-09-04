a4846e06c12a65cb57ff91c2225d27e43f5c5d04
Kristina Yin <kristinacyin@gmail.com>
2022-09-04 09:05:19 -0700

    finished assignment1

--- Week 3/assignment1.dart
+++ Week 3/assignment1.dart
@@ -3,6 +3,7 @@
  *  By Kristina Yin
 */
 
+import 'dart:core';
 import 'dart:math';
 
 enum Weather { rainy, cloudy, thunderstorm }
@@ -16,8 +17,24 @@ enum Food {
   tacos,
 }
 
-// convert enum to string
-String enumToString(Object o) => o.toString().split('.').last;
+// extension of Food enum
+extension FoodExtension on Food {
+  static const Map<Food, String> values = {
+    Food.Thai: "ate Thai food.",
+    Food.sandwhich: "made a ham sandwhich.",
+    Food.pizza: "ate pizza.",
+    Food.tacos: "ate tacos.",
+  };
+
+  String get value => values[this].toString();
+}
+
+// pick random value from Weather
+Weather randomWeather() {
+  final random = Random();
+  Weather weatherToday = Weather.values[random.nextInt(3)];
+  return weatherToday;
+}
 
 // choose food based on weather and day of the week
 Food decideFood(Day dayToday, Weather weatherToday) {
@@ -69,10 +86,38 @@ Food decideFood(Day dayToday, Weather weatherToday) {
   }
 }
 
-void main() {
-  Weather weatherToday = Weather.rainy;
-  Day dayToday = Day.Monday;
-
-  switch (weatherToday) {
+/*
+// print whole food statement based on Food enum
+String foodString(Food chosenFood) {
+  switch (chosenFood) {
+    case Food.Thai:
+      return "ate Thai food.";
+    case Food.sandwhich:
+      return "made a ham sandwhich.";
+    case Food.pizza:
+      return "ate pizza.";
+    case Food.tacos:
+      return "ate tacos.";
   }
+}
+*/
+
+// print line, using decideFood and randomWeather
+void printLine(Day today, Weather weatherToday) {
+  print(today.name +
+      " is " +
+      weatherToday.name +
+      ", so Sarah " +
+      decideFood(today, weatherToday).value);
+}
+
+void printWeeklyFood() {
+  for (int i = 0; i < 7; i++) {
+    Weather weatherToday = randomWeather();
+    printLine(Day.values[i], weatherToday);
+  }
+}
+
+void main() {
+  printWeeklyFood();
 }
